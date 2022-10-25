9df3dd5490714dad0b5f30a5d32a6eff08ec5ed8
Wesley Skeen <wesley.skeen@emailage.com>
2022-10-25 14:08:58 +0100

    First commit with basic test

---
+++ src/Basics.Tests/Tests.fs
@@ -0,0 +1,11 @@
+module Tests
+
+open System
+open FsUnit
+open Xunit
+open Basics.Functions
+
+[<Fact>]
+let ``Test function 'convertMilesToYards' should convert miles to yards`` () =
+    let result = 1.0 |> convertMilesToYards    
+    result |> should equal 1760
---
+++ src/Basics/Functions.fs
@@ -0,0 +1,8 @@
+module Basics.Functions
+
+[<Literal>]
+let milesToYardsMultiplyBy = 1760.0
+
+let convertMilesToYards (miles : float) : float =    
+    milesToYardsMultiplyBy * miles
+ 
